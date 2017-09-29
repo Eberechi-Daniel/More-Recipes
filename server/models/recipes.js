0b01ee3bbeb26512359e84cfc93d66496f57b431
Nwoge_Eberechi <eberechi.daniel@yahoo.com>
2017-09-29 03:27:19 +0100

    add recipes.js that contains the dummy data

---
+++ server/models/recipes.js
@@ -0,0 +1,25 @@
+const recipes = [{
+        id: 1,
+        title: 'Spaghetti Bolognese',
+        author: 'Nwoge Eberechi',
+        ingredients: [
+            'Onions', 'Pepper', 'Tomatoes'
+        ],
+        directions: 'Boil water for some minutes',
+        upvote: 2,
+        downvote: 1
+    },
+    {
+        id: 2,
+        title: 'Pepper Chicken',
+        author: 'Kingsley',
+        ingredients: [
+            'Full chicken', 'Pepper', 'Spices'
+        ],
+        directions: 'Mix your ....',
+        upvote: 4,
+        downvote: 1
+    }
+];
+
+export default recipes;
