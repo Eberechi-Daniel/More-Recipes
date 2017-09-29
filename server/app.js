770cd6bb7357a4abb8774fd6a0cff5f959f8788d
Nwoge_Eberechi <eberechi.daniel@yahoo.com>
2017-09-29 06:07:57 +0100

    add api routes to app.js file

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import recipes from './models/recipes';
 
 const app = express();
 
@@ -15,6 +16,46 @@ app.get('/api', (req, res) => {
         message: 'Welcome to More-Recipes'
     });
 });
-
+// API route that allows a user to get all recipes in the application
+app.get('/api/recipes', (req, res) => {
+    res.status(200);
+    res.send(recipes);
+});
+// API route that allows a user add a recipe
+app.post('/api/recipes', (req, res) => {
+    const item = req.body;
+    console.log(item.id);
+    if (!item.id) {
+        return res.sendStatus(500);
+    }
+    recipes.push(item);
+    res.send(recipes);
+});
+// API route that allows a user to modify a recipe
+app.put('/api/recipes/:id', (req, res) => {
+    const id = parseInt(req.params.id, 10);
+    console.log(id);
+    const existingItem = recipes.filter(r => r.id === id)[0];
+    if (!existingItem) {
+        const item = req.body;
+        item.id = id;
+        recipes.push(item);
+        res.setHeader('Location', '/api/recipes/', id);
+        res.sendStatus(201);
+    } else {
+        existingItem.name = req.body.name;
+        res.sendStatus(204);
+    }
+});
+// API route to delete recipe
+app.delete('/app/recipe/:id', (req, res) => {
+    const id = parseInt(req.params.id, 10);
+    const existingItem = recipes.filter(r => r.id === id)[0];
+    if (!existingItem) {
+        return res.sendStatus(404);
+    }
+    recipes = recipes.filter(r => r.id !== id);
+    res.sendStatus(204);
+});
 
 app.listen(port, () => console.log(`Application started on port ${port}`));
