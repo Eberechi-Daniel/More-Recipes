392bb18711403d916bfad7901007061be1d349f6
Nwoge_Eberechi <eberechi.daniel@yahoo.com>
2017-09-29 03:26:34 +0100

    add recipes.js file that manipulates the dummy data

---
+++ server/controllers/recipes.js
@@ -0,0 +1,14 @@
+import recipes from '../models/recipes';
+/**
+ * @class RecipesApiController
+ */
+
+class RecipesApiController {
+    /**
+     * Initializes data
+     * @param {obj} data
+     */
+    constructor(data) {
+        this.data = data;
+    }
+}
