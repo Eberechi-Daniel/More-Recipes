e89e9bcb635f0d5807aa96b645ca5aebdb635bce
Nwoge_Eberechi <eberechi.daniel@yahoo.com>
2017-09-29 06:08:43 +0100

    modify recipe.js file

--- server/controllers/recipes.js
+++ server/controllers/recipes.js
@@ -1,14 +1,1 @@
 import recipes from '../models/recipes';
-/**
- * @class RecipesApiController
- */
-
-class RecipesApiController {
-    /**
-     * Initializes data
-     * @param {obj} data
-     */
-    constructor(data) {
-        this.data = data;
-    }
-}
