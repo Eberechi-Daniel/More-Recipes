18c95b05395cd3fb1fa5b9b9352f656c2b1e87b5
Nwoge_Eberechi <eberechi.daniel@yahoo.com>
2017-10-17 16:31:05 +0100

    add model for review

---
+++ server/models/reviews.js
@@ -0,0 +1,33 @@
+const reviews = [{
+        recipeid: 1,
+        review: 'This recipe is nice, just tried it',
+        userid: 3,
+        vote: [
+            'upvote', 'downvote', 'views'
+        ]
+    },
+    {
+        recipeid: 1,
+        review: 'This recipe is nice, just tried it',
+        userid: 3,
+        vote: [
+            'upvote', 'downvote', 'views'
+        ]
+    },
+    {
+        recipeid: 1,
+        review: 'This recipe is nice, just tried it',
+        userid: 3,
+        vote: [
+            'upvote', 'downvote', 'views'
+        ]
+    },
+    {
+        recipeid: 1,
+        review: 'This recipe is nice, just tried it',
+        userid: 3,
+        vote: [
+            'upvote', 'downvote', 'views'
+        ]
+    },
+];
